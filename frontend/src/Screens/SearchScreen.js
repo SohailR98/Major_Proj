e6a11ebace52c886305f627e030aac504ad8b4f9
Sohail Rashed <sohailrr8@gmail.com>
2022-05-28 15:51:29 +0400

    new-title-logo

--- frontend/src/Screens/SearchScreen.js
+++ frontend/src/Screens/SearchScreen.js
@@ -148,7 +148,7 @@ export default function SearchScreen() {
   return (
     <div>
       <Helmet>
-        <title>Seacrh Products</title>
+        <title>Search Products</title>
       </Helmet>
       <Row>
         <Col md={3}>
